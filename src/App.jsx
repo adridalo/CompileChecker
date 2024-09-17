ad9532f176efffe14c66e8dfee9f0babecde7bad
adridalo <adrianodalonzo@gmail.com>
2024-09-17 00:31:34 -0400

    Add logic to check compilation on a source code file

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,51 @@ function App() {
   const ACCEPTED_EXTENSIONS = [".c", ".cpp", ".cs", ".go", ".java", ".js", ".py", ".r", ".ts"]
   const [selectedFile, setSelectedFile] = useState(undefined)
   const [fileUrl, setFileUrl] = useState(undefined)
+  const [compileStatus, setCompileStatus] = useState('')
 
-  useEffect(() => {
+  const compile = async () => {
     if(selectedFile !== undefined) {
+      setCompileStatus('Compiling...')
       const CODE_EVALUATION_URL = 'https://api.hackerearth.com/v4/partner/code-evaluation/submissions/'
+      const CLIENT_SECRET = import.meta.env['VITE_CLIENT_SECRET']
+
+      const fetchedFile = await fetch(selectedFile)
+      const fileContent = await fetchedFile.text()
+
+      const data = {
+        source: fileContent,
+        lang: determineLanguage(fileUrl),
+        id: import.meta.env['VITE_CLIENT_ID']
+      }
+      const headers = { 
+        'client-secret': CLIENT_SECRET,
+        'Content-Type': 'application/json'
+      }
+
+      const evaluationResponse = await fetch(CODE_EVALUATION_URL, {
+        headers: headers,
+        body: JSON.stringify(data),
+        method: 'POST'
+      })
+
+      if(evaluationResponse.ok) {
+        const evaluation = await evaluationResponse.json()
+        const evaluationUrl = evaluation['status_update_url']
+
+        const compileResponse = await fetch(evaluationUrl, {
+          headers: headers,
+          method: 'GET'
+        })
+
+        const compile = await compileResponse.json()
+        if(compile['result']['compile_status'] === 'OK') {
+          setCompileStatus(`Code compilation succeeded in ${compile['result']['run_status']['time_used']} seconds!`)
+        } else {
+          setCompileStatus(`Code compilation failed: ${compile['result']['compile_status']}`)
+        }
+      }
     }
-  }, [selectedFile])
+  }
 
   return (
     <>
@@ -48,7 +87,8 @@ function App() {
 
       <br></br>
 
-      <button>Compile</button>
+      <button onClick={compile}>Compile</button>
+      <p>{compileStatus}</p>
     </>
   )
 }
